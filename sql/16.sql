105deaca0148929e27f679e2104017e627870f12
ValentinaWatson <kwatson@hmc.edu>
2024-02-23 01:12:15 -0800

    finish

--- sql/16.sql
+++ sql/16.sql
@@ -4,3 +4,10 @@
  * Use tables payment, rental, inventory, and film. 
  */
 
+SELECT film.title, sum(pay.amount) AS "profit"
+FROM film
+JOIN inventory inv USING (film_id)
+JOIN rental r USING (inventory_id)
+JOIN payment pay USING (rental_id)
+GROUP BY film.title
+ORDER BY profit DESC;
